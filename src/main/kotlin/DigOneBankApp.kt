9c87fbac6e99864730d57a1d95f5834528274c4b
Leonardo Alcantara <leonardo.alcantara@escolaconquer.com.br>
2021-04-12 20:29:03 -0300

    Conclusão lição 9 - Fundamentos Orientação Objetos Kotlin

---
+++ src/main/kotlin/DigOneBankApp.kt
@@ -0,0 +1,26 @@
+package main.kotlin
+
+import kotlin.io.println
+
+class Pessoa {
+
+    var nome: String = "leonardo"
+    var cpf: String = "123.123.123.12"
+    private set
+
+    constructor()
+
+    fun info():String {
+        return "Nome $nome e cpf $cpf"
+    }
+}
+
+
+fun main() {
+    val pessoa = Pessoa()
+
+    println(pessoa.info())
+    println(pessoa.nome)
+    println(pessoa.cpf)
+
+}
