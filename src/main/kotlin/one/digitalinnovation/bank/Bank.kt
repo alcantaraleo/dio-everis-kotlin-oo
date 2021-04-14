b98592b4d014e74221fee184bd26463513ef15b9
Leonardo Alcantara <leonardo.alcantara@escolaconquer.com.br>
2021-04-13 22:44:37 -0300

    Pre-aula enum

---
+++ src/main/kotlin/one/digitalinnovation/bank/Bank.kt
@@ -0,0 +1,9 @@
+package main.kotlin.one.digitalinnovation.bank
+
+import javax.print.attribute.IntegerSyntax
+
+data class Bank(
+    val name: String,
+    val code: Int
+
+)
---
+++ src/main/kotlin/one/digitalinnovation/bank/tests/BancoTest.kt
@@ -0,0 +1,15 @@
+package one.digitalinnovation.bank.tests
+
+import main.kotlin.one.digitalinnovation.bank.Bank
+
+fun main() {
+    val bank = Bank(name = "Banco Digital One", code = 123)
+
+    println(bank.name)
+    println(bank.code)
+
+    val anotherBank = bank.copy("Banco Digital One Copy")
+
+    println(anotherBank.name)
+    println(anotherBank.code)
+}
